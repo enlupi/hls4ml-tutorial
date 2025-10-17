2c092020fd6df111bb130af747d4db77276ae45d
Enrico Lupi <enlupi@olhsw-01.cern.ch>
2025-10-17 16:36:08 +0200

    RUN part 6

---
+++ Models/model_4/hls4ml_prj/firmware/weights/b26.h
@@ -0,0 +1,16 @@
+//Numpy array shape [10]
+//Min -0.076246917248
+//Max 0.111602507532
+//Number of zeros 0
+
+#ifndef B26_H_
+#define B26_H_
+
+#ifndef __SYNTHESIS__
+output_dense_bias_t b26[10];
+#else
+output_dense_bias_t b26[10] = {-0.0509229191, -0.0379172526, -0.0390149988, 0.0804303512, 0.0679741204, 0.0323475301, 0.0199421626, -0.0762469172, 0.1116025075, -0.0286114886};
+
+#endif
+
+#endif
---
+++ Models/model_4_quant/hls4ml_prj/firmware/weights/b23.h
@@ -0,0 +1,16 @@
+//Numpy array shape [10]
+//Min -0.065754897892
+//Max 0.172885075212
+//Number of zeros 0
+
+#ifndef B23_H_
+#define B23_H_
+
+#ifndef __SYNTHESIS__
+output_dense_bias_t b23[10];
+#else
+output_dense_bias_t b23[10] = {-0.0657548979, -0.0057665831, 0.0260441788, 0.0765624642, 0.0074307905, -0.0520059541, -0.0596684031, 0.0072272974, 0.1728850752, 0.0511009544};
+
+#endif
+
+#endif
